b229f61105c51e6fdee5f1c2ceb5fb73949f592c
Donnie D'Amato <fauxserious@gmail.com>
2022-04-08 23:08:28 -0400

    Move

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addPassthroughCopy("src/*.(css|jpg|png|svg|webmanifest|ico)");
+  eleventyConfig.addPassthroughCopy({"src/public/*.(css|jpg|png|svg|webmanifest|ico)": '/'});
 
   return {
     // When a passthrough file is modified, rebuild the pages:
